71a430258fe5c9f530b5292411c8907df312dfe8
MajdHomsi <majd.homsi.ca@gmail.com>
2021-10-15 19:48:34 -0400

    Final

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
+/*********************************************************************************
+ *  WEB422 – Assignment 3
+ *  I declare that this assignment is my own work in accordance with Seneca Academic Policy.
+ *  No part of this assignment has been copied manually or electronically from any other source
+ *  (including web sites) or distributed to other students.
+ *
+ *  Name: Majd Homsi Student ID: 113692206  Date: 2021-10-15
+ *
+ *
+ ********************************************************************************/
+
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
