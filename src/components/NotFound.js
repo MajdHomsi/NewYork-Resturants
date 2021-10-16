d32a685aa5834377b1a2ac1a5527e2c1d09a4772
MajdHomsi <majd.homsi.ca@gmail.com>
2021-10-15 20:00:27 -0400

    Center

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,11 +1,13 @@
 function NotFound() {
   return (
-    <div>
-      <h1>Not Found</h1>
-      <p>
-        <h2>Well Well Well, are you looking for Narnia?</h2>
-      </p>
-    </div>
+    <center>
+      <div>
+        <h1>Not Found</h1>
+        <p>
+          <h2>Well Well Well, are you looking for Narnia?</h2>
+        </p>
+      </div>
+    </center>
   );
 }
 
